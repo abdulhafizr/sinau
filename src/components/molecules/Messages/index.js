124cf2b7974152b207a8174a4da269d171a8c158
ahr <abdulhafizr>
2021-03-25 08:41:24 +0700

    | Make Message Body Section

--- src/components/molecules/Messages/index.js
+++ src/components/molecules/Messages/index.js
@@ -1,4 +1,4 @@
-import {ICAddMessage, ICSearchMessage} from '../../../assets'
+import {ICAddMessage, ICSearchMessage, DMUser1} from '../../../assets'
 import './index.css'
 
 function Messages() {
@@ -11,6 +11,32 @@ function Messages() {
             <div className="messages__search">
                 <img src={ICSearchMessage} alt="Search"/>
                 <input type="text" placeholder="Search"/>
+            </div>
+            <div className="messages__body">
+                <div className="message">
+                    <img src={DMUser1} alt="Profile" className="message__profile"/>
+                    <div className="message__info">
+                        <h1 className="message__name">Nissa Sabyan</h1>
+                        <p className="message__history">How about number 3?</p>
+                        <span className="message__timestamp">10.15 pm</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src={DMUser1} alt="Profile" className="message__profile"/>
+                    <div className="message__info">
+                        <h1 className="message__name">Nissa Sabyan</h1>
+                        <p className="message__history">How about number 3?</p>
+                        <span className="message__timestamp">10.15 pm</span>
+                    </div>
+                </div>
+                <div className="message">
+                    <img src={DMUser1} alt="Profile" className="message__profile"/>
+                    <div className="message__info">
+                        <h1 className="message__name">Nissa Sabyan</h1>
+                        <p className="message__history">How about number 3?</p>
+                        <span className="message__timestamp">10.15 pm</span>
+                    </div>
+                </div>
             </div>
         </div>
     )
