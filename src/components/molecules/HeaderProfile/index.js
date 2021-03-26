18788b2da48f3ef9063e32465f9f6c1054be60a1
ahr <abdulhafizr>
2021-03-26 16:17:39 +0700

    | Make Profile Page

--- src/components/molecules/HeaderProfile/index.js
+++ src/components/molecules/HeaderProfile/index.js
@@ -2,13 +2,33 @@ import {DMUser1, ICEditProfile} from '../../../assets'
 import './index.css'
 
 function HeaderProfile() {
+    const changePhoto = (event) => {
+        const userPhoto = document.querySelector('.header__profile-avatar img')
+        const photo = event.target.files[0]
+        const reader  = new FileReader();
+
+        reader.onloadend = function () {
+            userPhoto.src = reader.result;
+        }
+        if (photo) {
+            reader.readAsDataURL(photo);
+        } else {
+            userPhoto.src = DMUser1;
+        }
+    }
     return (
         <div className="header__profile">
             <div className="header__profile-body">
                 <div className="header__profile-avatar">
                     <img src={DMUser1} alt="user"/> 
                     <div className="change__photo">
-                        <input type="file" id="change__photo" accept="image/*" hidden/>
+                        <input 
+                            type="file"
+                            onChange={changePhoto} 
+                            id="change__photo" 
+                            accept="image/*" 
+                            hidden
+                        />
                         <label htmlFor="change__photo"><img src={ICEditProfile} alt="edit"/></label>
                     </div>
                 </div>
--- src/pages/Content/Profile/index.js
+++ src/pages/Content/Profile/index.js
@@ -1,3 +1,4 @@
+import {ICPhoneNumber, ICChangePassword, ICChatSetting, ICPushNotification, ICPrivacyAndSecurity, ICDataAndStorage} from '../../../assets'
 import {HeaderProfile} from '../../../components'
 import './index.css'
 
@@ -5,6 +6,34 @@ function Profile() {
     return (
         <div className="profile">
             <HeaderProfile />
+            
+            <h1 className="profile__label">Profile Settings</h1>
+            <div className="profile__body">
+                <div className="profile__body-item">
+                    <img src={ICPhoneNumber} alt="phone number"/>
+                    <p className="profile__body-item-label">Phone Numbers</p>
+                </div>
+                <div className="profile__body-item">
+                    <img src={ICChangePassword} alt="change password"/>
+                    <p className="profile__body-item-label">Change Password</p>
+                </div>
+                <div className="profile__body-item">
+                    <img src={ICChatSetting} alt="chat setting"/>
+                    <p className="profile__body-item-label">Chat Settings</p>
+                </div>
+                <div className="profile__body-item">
+                    <img src={ICPushNotification} alt="push notification"/>
+                    <p className="profile__body-item-label">Push Notifications</p>
+                </div>
+                <div className="profile__body-item">
+                    <img src={ICPrivacyAndSecurity} alt="privacy and security"/>
+                    <p className="profile__body-item-label">Privacy and Security</p>
+                </div>
+                <div className="profile__body-item">
+                    <img src={ICDataAndStorage} alt="data and storage"/>
+                    <p className="profile__body-item-label">Data and Storage</p>
+                </div>
+            </div>
         </div>
     )
 }
